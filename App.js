6ffd3890fdabda41522f198020c72dfd4507f44b
Jude-raganit <juderaganit56@gmail.com>
2024-09-12 19:35:50 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+let x = 18
+let y = 9
+let isxgreaterthanyy= x > y;
+let isxlessthany = x < y;
+let isxgreaterthanorequaltoy = x >= y;
+let isxlessthanorequaltoy = x <= y;
+let isxstrictequalityy = x === y;
+let isxlooseequalityy = x == y;
+let isxstictinequalityy = x !== y;
+let isxlooseinequalityy = x != y;
+let isxandy = x && y;
+let isxory = x || y;
+let isxnoty = x ! y;
+
+console.log("is x greaterthan y?",isxgraeterthany);
+console.log("is x lessthan y?",isxlessthany);
+console.log("is x greaterthan or equal to y?",isxgreaterthanorequaltoy);
+console.log("is x lessthan or equal to y?",isxlessthany);
+console.log("is x strict equality y?",isxstrictequalityy);
+console.log("is x loose equality y?",isxlooseequalityy);
+console.log("is x stict inequality y?",isxstictinequalityy);
+console.log("is x loose inequality y?",isxlooseinequalityy);
+console.log("is x and y?",isxandy);
+console.log("is x or y?",isxory);
+console.log("is x not y?",isxnoty);
